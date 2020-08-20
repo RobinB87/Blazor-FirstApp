eecd97e03dba80c0af016faf30e3720d2e3e98e4
Bette, Robin <robin.bette@capgemini.com>
2020-08-20 17:48:40 +0200

    addProject

---
+++ MyMoviesApi/Data/MovieService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyMoviesApi.Data;
+
+namespace MyMoviesApi.Data
+{
+    public class MovieService
+    {
+        private readonly MoviesContext _context;
+        public MovieService(MoviesContext context)
+        {
+            _context = context;
+        }
+
+    //    public async Task<ActionResult<IEnumerable<Movie>> GetMoviesAsync()
+    //    {
+    //        return await _context.Movies.Any().ToListAsync();
+
+    //    //// Get Weather Forecasts  
+    //    //return await _context.WeatherForecast
+    //    //    // Only get entries for the current logged in user
+    //    //    .Where(x => x.UserName == strCurrentUser)
+    //    //    // Use AsNoTracking to disable EF change tracking
+    //    //    // Use ToListAsync to avoid blocking a thread
+    //    //    .AsNoTracking().ToListAsync();
+    //}
+    }
+}
---
+++ MyMoviesApi/Data/MoviesContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyMoviesApi.Data
+{
+    public class MoviesContext : DbContext
+    {
+        // Constructor is required
+        public MoviesContext(DbContextOptions<MoviesContext> options) : base(options) { }
+
+        // THe DbSet with movie model
+        public virtual DbSet<Movie> Movies { get; set; }
+
+        // For getting the default connection?
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("DefaultConnection");
+            }
+        }
+    }
+}
+
+//System.ArgumentException: 'AddDbContext was called with configuration, but the context type 'MoviesContext' only declares a parameterless constructor.
+//This means that the configuration passed to AddDbContext will never be used.
+//If configuration is passed to AddDbContext,
+//then 'MoviesContext' should declare a constructor that accepts a DbContextOptions<MoviesContext> and must pass it to the base constructor for DbContext.'
